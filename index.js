72929de6070ab9a0b25f15b81cc625e8cac3d0f8
Melaynee <melayneeyt@gmail.com>
2024-01-12 12:24:33 +0100

    PostModel created

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const User = require("./models/User.js");
+const Post = require("./models/Post.js");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+const multer = require("multer");
+const fs = require("fs");
 
 const app = express();
-
+const uploadMiddleware = multer({ dest: "./uploads" });
 const salt = bcrypt.genSaltSync(10);
 const secret = "9vCo4";
 
 app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use(express.json());
 app.use(cookieParser());
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 mongoose.connect(
   "mongodb+srv://blog:Cmm9vCo4kxL7O4FS@cluster0.sf29n96.mongodb.net/"
@@ -21,11 +25,12 @@ mongoose.connect(
 
 // Registration
 app.post("/registration", async (req, res) => {
-  const { email, password } = req.body;
+  const { email, username, password } = req.body;
 
   try {
     const userDoc = await User.create({
       email,
+      username,
       password: bcrypt.hashSync(password, salt),
     });
     res.json(userDoc);
@@ -62,6 +67,45 @@ app.get("/profile", async (req, res) => {
 
 app.post("/logout", async (req, res) => {
   res.cookie("token", "").json("ok");
+});
+
+app.post("/create", uploadMiddleware.single("file"), async (req, res) => {
+  const { originalname, path } = req.file;
+  const parts = originalname.split(".");
+  const ext = parts[parts.length - 1];
+  const newPath = path + "." + ext;
+
+  fs.renameSync(path, newPath);
+
+  const { token } = req.cookies;
+
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { title, summary, content } = req.body;
+    const postDoc = await Post.create({
+      title,
+      summary,
+      content,
+      cover: newPath,
+      author: info.id,
+    });
+    res.json({ postDoc });
+  });
+});
+
+app.get("/post", async (req, res) => {
+  const posts = await Post.find()
+    .populate("author", ["username"])
+    .sort({ createdAt: -1 })
+    .limit(20);
+
+  res.json(posts);
+});
+
+app.get("/post/:id", async (req, res) => {
+  const { id } = req.params;
+  const postDoc = await Post.findById(id).populate("author", ["username"]);
+  res.json(postDoc);
 });
 
 // Cmm9vCo4kxL7O4FS
--- models/User.js
+++ models/User.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true, min: 8 },
 });
 
-const UserModel = mongoose.model("User", UserSchema); 
+const UserModel = mongoose.model("User", UserSchema);
 
 module.exports = UserModel;
